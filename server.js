ffcccc6cd26eceb9751d0c0474fdc7395d9c6e81
mmrydz <megrydzewski@gmail.com>
2019-05-06 14:31:48 -0400

    mongoose

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/wpScraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/wpScraper";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
