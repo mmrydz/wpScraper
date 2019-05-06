a5c5d6a1451dcca2e50b3e313bcfd5cecf0c9ba7
mmrydz <megrydzewski@gmail.com>
2019-05-06 14:07:02 -0400

    readme

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href=" + data[i].link + ">Read</a></p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + "<strong>" + 
+    data[i].title + "</strong><br /><center>" + "<a href=" + data[i].link + ">Read</a></center></p>");
   }
 });
 
--- server.js
+++ server.js
@@ -39,13 +39,14 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(response.data);
 
     // Now, we grab every article url (since titles are children of urls on this site) and do the following:
-    $(".headline").each(function(i, element) {
+    $(".flex-stack").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children("a").text();
-      result.link = $(this).children("a").attr("href");
+      result.title = $(this).children(".headline").children("a").text().trim();
+      result.link = $(this).children(".headline").children("a").attr("href");
+     //result.blurb = $(this).children(".blurb").text().trim();
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
